76ace4fc8e93f695026ad572c85cb2660dd278cf
realEdgar <ing.edgar.lopez.28@gmail.com>
2021-04-25 09:14:45 -0500

    Solving some issues with the styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,28 @@ import React, { useState } from 'react';
 export const Header = () => {
     const [ darkMode, setDarkMode ] = useState(false);
 
-    //==> Creating a function to handle the click of the button
+    //===== Creating a function to handle the click of the button =====//
     function handleClick() {
-        if(darkMode) {
-            document.querySelector('body').classList.add('dark-mode');
+        const articles = document.querySelectorAll('article');
+        const body = document.querySelector('body');
+        const button = document.querySelector('button');
+        const header = document.querySelector('header');
+        if(!darkMode) {
+            body.classList.add('dark-mode');
+            button.style.border = '2px solid white';
+            button.style.color = 'white';
+            header.style.borderBottomColor = 'white'
+            for (let i = 0; i < articles.length; i++) {
+                articles[i].style.border = '2px solid white';
+            }
         } else {
-            document.querySelector('body').classList.remove('dark-mode');
+            body.classList.remove('dark-mode');
+            button.style.border = '2px solid black';
+            button.style.color = 'black';
+            header.style.borderBottomColor = 'black'
+            for (let i = 0; i < articles.length; i++) {
+                articles[i].style.border = '2px solid black';
+            }
         }        
         setDarkMode(!darkMode);
     }
@@ -16,7 +32,7 @@ export const Header = () => {
     return (
         <header>
             <h1>React Hooks</h1>
-            <button type="button" onClick={ handleClick } >{ darkMode ? 'Dark Mode' : 'Light Mode' }</button>
+            <button type="button" onClick={ handleClick } >{ darkMode ? 'Ligth Mode' : 'Dark Mode' }</button>
         </header>
     )
 }
